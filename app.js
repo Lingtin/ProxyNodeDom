4fe645955f5f9663ae4b0ea29f752a81a710a880
zxsxzt@foxmail.com <185740752a>
2018-04-08 18:27:47 +0800

    edit address

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var proxy = require('express-http-proxy');
 const serve   = require('express-static');
 
-app.use('/api', proxy('http://49.4.12.144:8080/'))
+app.use('/api', proxy('代理地址'))
 app.use(serve(__dirname + '/www'));
 
 app.listen(8000, function(){
